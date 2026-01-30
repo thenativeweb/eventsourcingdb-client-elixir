6ba00e0ca27500f073c6b316a49a644e9c18dc88
gossi <gossi@users.noreply.github.com>
2026-01-30 15:43:16 +0100

    fix tests

--- test/essentials_test.exs
+++ test/essentials_test.exs
@@ -1,5 +1,5 @@
 defmodule EventsourcingdbTest.Essentials do
-  alias Eventscourcingdb.Client
+  alias Eventsourcingdb.Client
   alias Eventsourcingdb.TestContainer
   use ExUnit.Case
 
@@ -8,27 +8,27 @@ defmodule EventsourcingdbTest.Essentials do
   container(:esdb, TestContainer.new(), shared: true)
 
   test "ping", %{esdb: esdb} do
-    assert Eventscourcingdb.ping(TestContainer.get_client(esdb)) == :ok
+    assert Eventsourcingdb.ping(TestContainer.get_client(esdb)) == :ok
   end
 
   test "ping unavailable server" do
     client =
       Client.new(base_url: "http://localhost:12345", api_token: "secrettoken", retry: false)
 
-    result = Eventscourcingdb.ping(client)
+    result = Eventsourcingdb.ping(client)
 
     assert match?({:error, :transmission_error, _}, result)
   end
 
   test "verify api token", %{esdb: esdb} do
-    assert Eventscourcingdb.verify_api_token(TestContainer.get_client(esdb)) == :ok
+    assert Eventsourcingdb.verify_api_token(TestContainer.get_client(esdb)) == :ok
   end
 
   test "verify invalid api token", %{esdb: esdb} do
     client = TestContainer.get_client(esdb)
     invalid_client = Client.new(client.base_url, "invalidtoken")
 
-    result = Eventscourcingdb.verify_api_token(invalid_client)
+    result = Eventsourcingdb.verify_api_token(invalid_client)
 
     assert match?({:error, :api_token_invalid}, result)
   end
