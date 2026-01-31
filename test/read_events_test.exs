95b3cef622564c4d83b024d2051ae4b4eefc1e0c
gossi <gossi@users.noreply.github.com>
2026-01-31 18:26:19 +0100

    one testcontainer instance works, do 2?

--- test/read_events_test.exs
+++ test/read_events_test.exs
@@ -16,184 +16,184 @@ defmodule EventsourcingdbTest.ReadEvents do
     assert Enum.count(events) == 0
   end
 
-  test "make read call with event", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "make read call with event", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    written =
-      Eventsourcingdb.write_events!(client, [create_test_eventcandidate("/test", %{"value" => 1})])
+  #   written =
+  #     Eventsourcingdb.write_events!(client, [create_test_eventcandidate("/test", %{"value" => 1})])
 
-    events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
+  #   events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
 
-    assert events == written
-  end
+  #   assert events == written
+  # end
 
-  test "make read call with multiple events", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "make read call with multiple events", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidates = create_numbered_eventcandidates(10)
+  #   event_candidates = create_numbered_eventcandidates(10)
 
-    written =
-      Eventsourcingdb.write_events!(client, event_candidates)
+  #   written =
+  #     Eventsourcingdb.write_events!(client, event_candidates)
 
-    events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
+  #   events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
 
-    assert events == written
-  end
+  #   assert events == written
+  # end
 
-  test "read from exact topic", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read from exact topic", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidate = create_test_eventcandidate("/test", %{"value" => 1})
+  #   event_candidate = create_test_eventcandidate("/test", %{"value" => 1})
 
-    Eventsourcingdb.write_events!(client, [event_candidate])
-    Eventsourcingdb.write_events!(client, [create_test_eventcandidate("/wrong", %{"value" => 1})])
+  #   Eventsourcingdb.write_events!(client, [event_candidate])
+  #   Eventsourcingdb.write_events!(client, [create_test_eventcandidate("/wrong", %{"value" => 1})])
 
-    events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
+  #   events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
 
-    assert length(events) == 1
-    assert assert_event_match_eventcandidate(Enum.at(events, 0), event_candidate, nil, nil)
-  end
+  #   assert length(events) == 1
+  #   assert assert_event_match_eventcandidate(Enum.at(events, 0), event_candidate, nil, nil)
+  # end
 
-  test "read recursive", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read recursive", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidate_parent = create_test_eventcandidate("/test", %{"value" => 1})
-    event_candidate_child = create_test_eventcandidate("/test/sub", %{"value" => 2})
+  #   event_candidate_parent = create_test_eventcandidate("/test", %{"value" => 1})
+  #   event_candidate_child = create_test_eventcandidate("/test/sub", %{"value" => 2})
 
-    written =
-      Eventsourcingdb.write_events!(client, [event_candidate_parent, event_candidate_child])
+  #   written =
+  #     Eventsourcingdb.write_events!(client, [event_candidate_parent, event_candidate_child])
 
-    events =
-      Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{recursive: true})
-      |> Enum.to_list()
+  #   events =
+  #     Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{recursive: true})
+  #     |> Enum.to_list()
 
-    assert events == written
-  end
+  #   assert events == written
+  # end
 
-  test "read not recursive", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read not recursive", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidate_parent = create_test_eventcandidate("/test", %{"value" => 1})
-    event_candidate_child = create_test_eventcandidate("/test/sub", %{"value" => 2})
+  #   event_candidate_parent = create_test_eventcandidate("/test", %{"value" => 1})
+  #   event_candidate_child = create_test_eventcandidate("/test/sub", %{"value" => 2})
 
-    Eventsourcingdb.write_events!(client, [event_candidate_parent, event_candidate_child])
+  #   Eventsourcingdb.write_events!(client, [event_candidate_parent, event_candidate_child])
 
-    events =
-      Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{recursive: false})
-      |> Enum.to_list()
+  #   events =
+  #     Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{recursive: false})
+  #     |> Enum.to_list()
 
-    assert Enum.count(events) == 1
-    assert assert_event_match_eventcandidate(Enum.at(events, 0), event_candidate_parent, nil, nil)
-  end
+  #   assert Enum.count(events) == 1
+  #   assert assert_event_match_eventcandidate(Enum.at(events, 0), event_candidate_parent, nil, nil)
+  # end
 
-  test "read chronological", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read chronological", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidates = create_numbered_eventcandidates(10)
+  #   event_candidates = create_numbered_eventcandidates(10)
 
-    written =
-      Eventsourcingdb.write_events!(client, event_candidates)
+  #   written =
+  #     Eventsourcingdb.write_events!(client, event_candidates)
 
-    events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
+  #   events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list()
 
-    assert events == written
-  end
+  #   assert events == written
+  # end
 
-  test "read anti-chronological", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read anti-chronological", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidates = create_numbered_eventcandidates(10)
+  #   event_candidates = create_numbered_eventcandidates(10)
 
-    written =
-      Eventsourcingdb.write_events!(client, event_candidates)
+  #   written =
+  #     Eventsourcingdb.write_events!(client, event_candidates)
 
-    events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list() |> Enum.reverse()
+  #   events = Eventsourcingdb.read_events(client, "/test") |> Enum.to_list() |> Enum.reverse()
 
-    assert events != written
-  end
+  #   assert events != written
+  # end
 
-  test "reads with lower bound", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "reads with lower bound", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    first_event = create_test_eventcandidate("/test", %{"value" => 23})
-    second_event = create_test_eventcandidate("/test", %{"value" => 42})
+  #   first_event = create_test_eventcandidate("/test", %{"value" => 23})
+  #   second_event = create_test_eventcandidate("/test", %{"value" => 42})
 
-    Eventsourcingdb.write_events!(client, [first_event, second_event])
+  #   Eventsourcingdb.write_events!(client, [first_event, second_event])
 
-    events =
-      Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
-        lower_bound: %BoundOptions{
-          id: "1",
-          type: :inclusive
-        },
-        recursive: false
-      })
+  #   events =
+  #     Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
+  #       lower_bound: %BoundOptions{
+  #         id: "1",
+  #         type: :inclusive
+  #       },
+  #       recursive: false
+  #     })
 
-    assert Enum.count(events) == 1
-    assert Enum.at(events, 0).data["value"] == 42
-  end
+  #   assert Enum.count(events) == 1
+  #   assert Enum.at(events, 0).data["value"] == 42
+  # end
 
-  test "reads with upper bound", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "reads with upper bound", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    first_event = create_test_eventcandidate("/test", %{"value" => 23})
-    second_event = create_test_eventcandidate("/test", %{"value" => 42})
+  #   first_event = create_test_eventcandidate("/test", %{"value" => 23})
+  #   second_event = create_test_eventcandidate("/test", %{"value" => 42})
 
-    Eventsourcingdb.write_events!(client, [first_event, second_event])
+  #   Eventsourcingdb.write_events!(client, [first_event, second_event])
 
-    events =
-      Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
-        upper_bound: %BoundOptions{
-          id: "0",
-          type: :inclusive
-        },
-        recursive: false
-      })
+  #   events =
+  #     Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
+  #       upper_bound: %BoundOptions{
+  #         id: "0",
+  #         type: :inclusive
+  #       },
+  #       recursive: false
+  #     })
 
-    assert Enum.count(events) == 1
-    assert Enum.at(events, 0).data["value"] == 23
-  end
+  #   assert Enum.count(events) == 1
+  #   assert Enum.at(events, 0).data["value"] == 23
+  # end
 
-  test "read everything from missing latest event", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read everything from missing latest event", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidates = create_numbered_eventcandidates(10)
-    Eventsourcingdb.write_events!(client, event_candidates)
+  #   event_candidates = create_numbered_eventcandidates(10)
+  #   Eventsourcingdb.write_events!(client, event_candidates)
 
-    events =
-      Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
-        from_latest_event: %FromLatestEventOptions{
-          subject: "/",
-          type: "io.eventsourcingdb.test.does-not-exist",
-          if_event_is_missing: :read_nothing
-        },
-        recursive: false
-      })
+  #   events =
+  #     Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
+  #       from_latest_event: %FromLatestEventOptions{
+  #         subject: "/",
+  #         type: "io.eventsourcingdb.test.does-not-exist",
+  #         if_event_is_missing: :read_nothing
+  #       },
+  #       recursive: false
+  #     })
 
-    assert Enum.count(events) == 0
-  end
+  #   assert Enum.count(events) == 0
+  # end
 
-  test "read from last event", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
+  # test "read from last event", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
 
-    event_candidates = create_numbered_eventcandidates(10)
-    Eventsourcingdb.write_events!(client, event_candidates)
+  #   event_candidates = create_numbered_eventcandidates(10)
+  #   Eventsourcingdb.write_events!(client, event_candidates)
 
-    Eventsourcingdb.write_events!(client, [create_test_eventcandidate("/marker", %{"value" => 1})])
+  #   Eventsourcingdb.write_events!(client, [create_test_eventcandidate("/marker", %{"value" => 1})])
 
-    written = Eventsourcingdb.write_events!(client, event_candidates)
+  #   written = Eventsourcingdb.write_events!(client, event_candidates)
 
-    events =
-      Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
-        from_latest_event: %FromLatestEventOptions{
-          subject: "/marker",
-          type: "io.eventsourcingdb.test",
-          if_event_is_missing: :read_nothing
-        },
-        recursive: false
-      })
-      |> Enum.to_list()
+  #   events =
+  #     Eventsourcingdb.read_events(client, "/test", %ReadEventsOptions{
+  #       from_latest_event: %FromLatestEventOptions{
+  #         subject: "/marker",
+  #         type: "io.eventsourcingdb.test",
+  #         if_event_is_missing: :read_nothing
+  #       },
+  #       recursive: false
+  #     })
+  #     |> Enum.to_list()
 
-    assert events == written
-  end
+  #   assert events == written
+  # end
 end
