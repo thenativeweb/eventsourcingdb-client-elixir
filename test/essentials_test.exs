e2a0444581c5ec49eb321ed654e65b7c0109b2be
gossi <gossi@users.noreply.github.com>
2026-02-02 15:39:31 +0100

    get some debugging info for testcontainers

--- test/essentials_test.exs
+++ test/essentials_test.exs
@@ -8,28 +8,29 @@ defmodule EventsourcingdbTest.Essentials do
   container(:esdb, TestContainer.new(), shared: true)
 
   test "ping", %{esdb: esdb} do
+    IO.inspect(TestContainer.get_base_url(esdb), label: "container base url")
     assert Eventsourcingdb.ping(TestContainer.get_client(esdb)) == :ok
   end
 
-  test "ping unavailable server" do
-    client =
-      Client.new(base_url: "http://localhost:12345", api_token: "secrettoken", retry: false)
+  # test "ping unavailable server" do
+  #   client =
+  #     Client.new(base_url: "http://localhost:12345", api_token: "secrettoken", retry: false)
 
-    result = Eventsourcingdb.ping(client)
+  #   result = Eventsourcingdb.ping(client)
 
-    assert match?({:error, :transmission_error, _}, result)
-  end
+  #   assert match?({:error, :transmission_error, _}, result)
+  # end
 
-  test "verify api token", %{esdb: esdb} do
-    assert Eventsourcingdb.verify_api_token(TestContainer.get_client(esdb)) == :ok
-  end
+  # test "verify api token", %{esdb: esdb} do
+  #   assert Eventsourcingdb.verify_api_token(TestContainer.get_client(esdb)) == :ok
+  # end
 
-  test "verify invalid api token", %{esdb: esdb} do
-    client = TestContainer.get_client(esdb)
-    invalid_client = Client.new(client.base_url, "invalidtoken")
+  # test "verify invalid api token", %{esdb: esdb} do
+  #   client = TestContainer.get_client(esdb)
+  #   invalid_client = Client.new(client.base_url, "invalidtoken")
 
-    result = Eventsourcingdb.verify_api_token(invalid_client)
+  #   result = Eventsourcingdb.verify_api_token(invalid_client)
 
-    assert match?({:error, :api_token_invalid}, result)
-  end
+  #   assert match?({:error, :api_token_invalid}, result)
+  # end
 end
