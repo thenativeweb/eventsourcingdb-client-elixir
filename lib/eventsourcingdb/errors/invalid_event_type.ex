486a39def68e7de1a12500ed2569c3e77a8963c2
gossi <gossi@users.noreply.github.com>
2026-03-10 12:23:47 +0100

    Improve exceptions

--- lib/eventsourcingdb/errors/invalid_event_type.ex
+++ lib/eventsourcingdb/errors/invalid_event_type.ex
@@ -1,4 +1,3 @@
-# credo:disable-for-lines:1
 defmodule Eventsourcingdb.Errors.InvalidEventType do
   defexception message: "Invalid Event Type"
 
--- lib/eventsourcingdb/errors/ping_failed.ex
+++ lib/eventsourcingdb/errors/ping_failed.ex
@@ -1,4 +1,3 @@
-# credo:disable-for-lines:1
 defmodule Eventsourcingdb.Errors.PingFailed do
   defexception message: "Ping Failed: Eventsourcingdb is not reachable"
 
