449a7a608e534f94025258113b00937787882294
goloroden <hello@thenativeweb.io>
2026-03-11 07:14:12 +0000

    chore: Bump version to 0.6.0. [skip ci]

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule EventSourcingDB.MixProject do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
   @source_url "https://github.com/thenativeweb/eventsourcingdb-client-elixir"
   @homepage_url "https://www.eventsourcingdb.io/"
   @documentation_url "https://docs.eventsourcingdb.io/"
